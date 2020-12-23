5f379141e965e82abe506c573e98d09a96203483
Nick Thompson <nathomp3@ncsu.edu>
2020-12-22 21:11:08 -0500

    removed adam AGFAIN

--- modules/index.js
+++ modules/index.js
@@ -10,4 +10,3 @@ exports.User = require('./user.js');
 exports.Examine = require('./examine.js');
 exports.Game = require('./game.js');
 exports.New = require('./new.js');
-exports.Adam = require('./adam.js');
