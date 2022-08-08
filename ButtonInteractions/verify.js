6f28edfd573d00b7bd09afb5ebb9bf8e7c81af0b
nathomp3 <nathomp3@ncsu.edu>
2022-08-07 21:41:32 -0400

    added role

--- ButtonInteractions/verify.js
+++ ButtonInteractions/verify.js
@@ -6,13 +6,14 @@ const tokens = require("../index.js");
 module.exports = {
     data: "verify",
     async execute(params) {
-        const { interaction } = params;
+        const { interaction, client } = params;
         const {discordID, srcID} = JSON.parse(interaction.customId);
         tokens.db.db("accounts").collection("users").insertOne({
             discordID,
             srcID,
             minecraftUUIDs: [],
         });
+        await interaction.guild.members.cache.get(discordID).roles.add('1006010384064458763');
         interaction.message.embeds[0].fields.push({ name: "Verified by:", value: `<@${interaction.user.id}>`});
         await interaction.update({
             embeds: interaction.message.embeds,
