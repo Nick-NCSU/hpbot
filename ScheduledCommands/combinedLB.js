aca64717c7c41a2e2b8b0c0e0b691dd6c8e68c82
nathomp3 <nathomp3@ncsu.edu>
2022-11-30 19:26:31 -0500

    Update combined lb to better support custom categories

--- ScheduledCommands/combinedLB.js
+++ ScheduledCommands/combinedLB.js
@@ -14,7 +14,7 @@ module.exports = {
     await findPlayers("m1z9l2d0", "wdmlzyxk", { "wl33656l": "mln04koq" }, "3v3v3v3");
     await findPlayers("m1z9l2d0", "vdom0912", { "wlex6zx8": "9qj46j7q" }, "4v4v4v4");
     await findPlayers("m1z9l2d0", "wkpm70jk", { "wlexj3x8": "klrym0jq" }, "4v4");
-    await updateRuns("m1z9l2d0", "zd3q41ek", { "yn2m5ye8": "5q8yjpkl" }, client);
+    await updateRuns("m1z9l2d0", "zd3q41ek", { "yn2m5ye8": "5q8yjpkl" }, 5, client);
 
     players = [];
     await findPlayers("m1z9l2d0", "824m59e2", {}, "Solo");
@@ -22,7 +22,7 @@ module.exports = {
     await findPlayers("m1z9l2d0", "wdmlzyxk", { "wl33656l": "4qyyv86q" }, "3v3v3v3");
     await findPlayers("m1z9l2d0", "vdom0912", { "wlex6zx8": "810mo3ol" }, "4v4v4v4");
     await findPlayers("m1z9l2d0", "wkpm70jk", { "wlexj3x8": "21d3wy4q" }, "4v4");
-    await updateRuns("m1z9l2d0", "zd3q41ek", { "yn2m5ye8": "0q50vd21" }, client);
+    await updateRuns("m1z9l2d0", "zd3q41ek", { "yn2m5ye8": "0q50vd21" }, 5, client);
   },
 };
 
@@ -40,37 +40,34 @@ async function findPlayers(game, category, vars, mode) {
       if (player.rel == "user") {
         // If the player already exists in players then find
         // them and update their time if needed.
-        if (players.some(p => p.id == player.id)) {
-          for (const record of players) {
-            if (record.id == player.id) {
-              if (!record[mode]) {
-                record[mode] = {
-                  time: run.run.times.primary_t,
-                  link: run.run.weblink
-                };
-              } else if (run.run.times.primary_t < record[mode]) {
-                record[mode].time = run.run.times.primary_t;
-                record[mode].link = run.run.weblink;
-              }
-              break;
-            }
-          }
-        } else {
-          let p = {
-            id: player.id,
-            [mode]: {
+        const existingPlayer = players.find((p) => p.id === player.id);
+        if (existingPlayer) {
+          if (!existingPlayer.runs[mode]) {
+            existingPlayer.runs[mode] = {
               time: run.run.times.primary_t,
               link: run.run.weblink
+            };
+          } else if (run.run.times.primary_t < existingPlayer.runs[mode]) {
+            existingPlayer.runs[mode].time = run.run.times.primary_t;
+            existingPlayer.runs[mode].link = run.run.weblink;
+          }
+        } else {
+          players.push({
+            id: player.id,
+            runs: {
+              [mode]: {
+                time: run.run.times.primary_t,
+                link: run.run.weblink
+              }
             }
-          };
-          players.push(p);
+          });
         }
       }
     }
   }
 }
 
-async function updateRuns(game, category, vars, client) {
+async function updateRuns(game, category, vars, count, client) {
   const channel = await client.channels.cache.get("1022357372854870076");
   let date = new Date().toISOString().slice(0, 10);
   let embed = new EmbedBuilder()
@@ -83,15 +80,10 @@ async function updateRuns(game, category, vars, client) {
   const varString = varMap.length ? `?${varMap.map(([variable, option]) => `var-${variable}=${option}`).join("&")}` : "";
 
   const data = await tokens.fetch(`https://www.speedrun.com/api/v1/leaderboards/${game}/category/${category}${varString}`);
+  console.log(JSON.stringify(players.find(p => p.id === 'x3554nqj')));
   // Filters out only the players with a time in every category
-  players = players.filter(function (player) {
-    return Object.prototype.hasOwnProperty.call(player, "Solo") &&
-      Object.prototype.hasOwnProperty.call(player, "Doubles") &&
-      Object.prototype.hasOwnProperty.call(player, "3v3v3v3") &&
-      Object.prototype.hasOwnProperty.call(player, "4v4v4v4") &&
-      Object.prototype.hasOwnProperty.call(player, "4v4");
-  });
-
+  players = players.filter(player => Object.keys(player.runs).length === count);
+  console.log(JSON.stringify(players, null, 2));
   embed = new EmbedBuilder()
     .setColor("#118855")
     .setTitle("Found " + players.length + " players with runs in all categories")
@@ -103,7 +95,7 @@ async function updateRuns(game, category, vars, client) {
   // Iterates through each player
   for (const player of players) {
     // Adds up their time
-    const total = (player["Solo"].time + player["Doubles"].time + player["3v3v3v3"].time + player["4v4v4v4"].time + player["4v4"].time).toFixed(3);
+    const total = Object.values(player.runs).reduce((total, run) => total + run.time, 0).toFixed(3);
     // Goes through the runs and if any have a faster or same time then
     // skip this player.
     for (const run of data.data.runs) {
@@ -129,7 +121,7 @@ async function updateRuns(game, category, vars, client) {
           }
         ],
         emulated: false,
-        comment: `Solo: ${player["Solo"].link}\nDoubles: ${player["Doubles"].link}\n3v3v3v3: ${player["3v3v3v3"].link}\n4v4v4v4: ${player["4v4v4v4"].link}\n4v4: ${player["4v4"].link}`,
+        comment: Object.entries(player.runs).map(([mode, info]) => `${mode}: ${info.link}`).join("\n"),
         variables: varMap.reduce((variables, variable) => {
           variables[variable[0]] = {
             type: "pre-defined",
@@ -139,13 +131,14 @@ async function updateRuns(game, category, vars, client) {
         }, {})
       }
     };
+    console.log(JSON.stringify(run, null, 2));
     // Submits the run
-    const submittedRun = await tokens.post("https://www.speedrun.com/api/v1/runs", {
-      method: "post",
-      body: JSON.stringify(run),
-      headers: { "Content-Type": "application/json", "X-API-Key": tokens.src }
-    });
-    weblinks.push(submittedRun.data?.weblink);
+    // const submittedRun = await tokens.post("https://www.speedrun.com/api/v1/runs", {
+    //   method: "post",
+    //   body: JSON.stringify(run),
+    //   headers: { "Content-Type": "application/json", "X-API-Key": tokens.src }
+    // });
+    // weblinks.push(submittedRun.data?.weblink);
   }
   embed = new EmbedBuilder()
     .setColor("#118855")
